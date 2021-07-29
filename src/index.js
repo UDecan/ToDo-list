fe8e6cbd8bee3803103bcf7c2b320cd935e9a385
Decan <foparu68@gmail.com>
2021-07-29 18:48:33 +0500

    Для heroku

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ const express = require("express");
 const config = require("config");
 const pg = require("pg");
 const router = require('./routes/index_router');
+const path = require('path')
+
 
 const app = express();
-const PORT = config.get('port') || 5000;
+const PORT = process.env.PORT || config.get('port') || 5000;
 
 app.use(express.json());
 // /api/user/authorize
 app.use('/api', router);
 
-// app.get("*", (req, res) => {
-//   return res.json({ status: "ok" });
-// });
+app.use('/', express.static(path.join(__dirname, '../', 'todo-list', 'build')))
+
+app.get('*', (req, res) => {
+  console.log(req.url)
+  res.sendFile(path.resolve(__dirname, '../', 'todo-list', 'build', 'index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Aplication has been started on port ${PORT}...`);

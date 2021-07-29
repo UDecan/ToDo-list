2d97c0b46cd27e440c4cc60037f73ee06d5b7676
Decan <foparu68@gmail.com>
2021-07-29 19:17:33 +0500

    Heroku

--- knexfile.js
+++ knexfile.js
@@ -35,16 +35,21 @@ module.exports = {
     }
   },
 
-
   production: {
     client: 'postgresql',
-    connection: 'postgres://ajbzgtaahqjmtq:bd14d5f386b8af03ce13ff649a2f63a05fe8528198d714c2a883f3e0ae655832@ec2-54-195-76-73.eu-west-1.compute.amazonaws.com:5432/d9j8oc32nodit8?ssl=true',
+    connection: {
+      host: 'ec2-54-195-76-73.eu-west-1.compute.amazonaws.com',
+      database: 'd9j8oc32nodit8',
+      user: 'ajbzgtaahqjmtq',
+      password: 'bd14d5f386b8af03ce13ff649a2f63a05fe8528198d714c2a883f3e0ae655832',
+      port: '5432',
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    },
     pool: {
       min: 2,
       max: 10
-    },
-    ssl: {
-      rejectUnauthorized: false,
     },
     migrations: {
       tableName: 'knex_migrations'
